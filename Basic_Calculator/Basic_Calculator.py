c4ad23149e0a15bcacbbc8697c0e676c55e6eed1
TaisaKiarkhi <tayisiyakiarkhi@gmail.com>
2023-09-11 18:08:19 +0200

    Add project files.

---
+++ Basic_Calculator/Basic_Calculator.py
@@ -0,0 +1,15 @@
+print("Hello! This is a basic calculator!")
+print("Please, enter your number")
+
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
+result = num_1 + num_2
+
+print("The result is: " + str(result))
+
+#Another way for the input, but we need to conert strings to  ints
+#num_1 = input("Enter the first number: ")
+#num_2 = input("Enter the second number: ")
+#result = int (num_1) + int(num_2)
+#print("The result is: " + str(result))
